aa318290f9e5c00cc5d124b255790b962e1d5d5e
John Doe <betiaman@example.com>
2023-06-06 16:56:17 -0500

    get even answered

--- lab2-answers.js
+++ lab2-answers.js
@@ -3,3 +3,11 @@
 for (let i = 0; i < 20; i++) {
     console.log(i);
 };
+
+// GET EVEN
+// Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i++ ) {
+    if( i % 2 == 0) {
+        console.log(i);
+    }
+}
