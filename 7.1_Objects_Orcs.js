03d9091ac936a637f2a5f6eae3ef689d8138d87d
John Doe <betiaman@example.com>
2023-06-12 12:57:56 -0500

    LAB 7.1 combining objects, arrays, and functions

--- 7.1_Objects_Orcs.js
+++ 7.1_Objects_Orcs.js
@@ -55,20 +55,87 @@
 //console.log(adventurer.companion.companion.type);
 
 //ARRAY WITHIN AN OBJECT WITHIN AN OBJECT WITHIN AN OBJECT...
-const adventurer = {
-	name: 'Timothy',
-	hitpoints: 10,
-	belongings: ["sword", "potion", "Tums"],
-	companion: {
-		name: "Velma",
-		type: "Bat",
-		companion: {
-			name: "Tim",
-			type: "Parasite",
-			belongings: ["SCUBA tank", "Rogan josh", "health insurance"]
-		}  
-	}
-}
+// const adventurer = {
+// 	name: 'Timothy',
+// 	hitpoints: 10,
+// 	belongings: ["sword", "potion", "Tums"],
+// 	companion: {
+// 		name: "Velma",
+// 		type: "Bat",
+// 		companion: {
+// 			name: "Tim",
+// 			type: "Parasite",
+// 			belongings: ["SCUBA tank", "Rogan josh", "health insurance"]
+// 		}  
+// 	}
+// }
 
 //What would your write to console.log "health insurance"?
-console.log(adventurer.companion.companion.belongings[adventurer.companion.companion.belongings.indexOf('health insurance')]);
+//console.log(adventurer.companion.companion.belongings[adventurer.companion.companion.belongings.indexOf('health insurance')]);
+
+//AN ARRAY OF OBJECTS
+const movies = [ 
+    { title: "Tokyo Story" },  
+    { title: "Paul Blart: Mall Cop" }, 
+    { title: "L'Avventura" } 
+];
+
+//referencing objects within array using index
+//console.log(movies[0]);
+
+//You could reference the properties by first asking for the index, then the property:
+//console.log(movies[0].title);
+
+//You could loop over the array and just print all of the titles:
+// for (let i=0; i < movies.length; i++) {
+// 	console.log(movies[i].title);
+// }
+
+//COMBINING OBJECTS, ARRAYS, AND FUNCTIONS
+
+//you can create a property for an object that is an array
+// const foo = {
+//     someArray:[1,2,3]
+// };
+//console.log(foo.someArray[0]); // 1
+
+//You can create a property for an object that is an object:
+// const foo = {
+//     someObject: {
+//         someProperty: 'oh hai!'
+//     }
+// };
+// console.log(foo.someObject.someProperty); // oh hai!
+
+//You can create a property for an object that is an function(method):
+// const foo = {
+//     someMethod:()=>{
+//         //console.log('oh hai');
+//         return 'oh hai '
+//     }
+// };
+//console.log(foo.someMethod());// logs 'oh hai!'
+
+//You can store an object in an array:
+// const foo = [{someProperty:'weee'}, 2, 3];
+// console.log(foo[0].someProperty);
+
+//You can store an array in an array:
+// const foo = [
+//     ["0,0", "0,1", "0,2"],
+//     ["1,0", "1,1", "1,2"],
+//     ["2,0", "2,1", "2,2"]
+// ];
+// console.log(foo[1][2]); //1,2
+
+//You can store a function in an array:
+const foo = [
+    1,
+    "hi",
+    ()=>{
+        console.log('fun');
+        return 'fun';
+    }
+];
+
+console.log(foo[2]());
