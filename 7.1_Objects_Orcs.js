172deecc9df103f028773ab2552866cd46a98152
John Doe <betiaman@example.com>
2023-06-12 12:14:42 -0500

    object within and object

--- 7.1_Objects_Orcs.js
+++ 7.1_Objects_Orcs.js
@@ -1,17 +1,37 @@
 // ARRAYS WITHIN OBJECTS
+// const adventurer = {
+// 	name: "Timothy",
+// 	hitpoints: 10,
+// 	belongings: ["sword", "potion", "Tums"]
+// }
+
+//Access all values in the adventurer.belongings array:
+//console.log(adventurer.belongings);
+
+//Access a specific item in the belongings array:
+//console.log(adventurer.belongings[0]);
+
+//ITERATE OVER AN ARRAY WITHIN AN OBJECT
+// for (let i=0; i < adventurer.belongings.length; i++) {
+// 	console.log(adventurer.belongings[i]);
+// }
+
+//OBJECT WITHIN AN OBJECT
 const adventurer = {
 	name: "Timothy",
 	hitpoints: 10,
-	belongings: ["sword", "potion", "Tums"]
+	belongings: ["sword", "potion", "Tums"],
+	companion: {
+		name: "Velma",
+		type: "Bat"
+	}
 }
 
-//Access all values in the adventurer.belongings array:
-console.log(adventurer.belongings);
+//Access the companion object:
+console.log(adventurer.companion);
 
-//Access a specific item in the belongings array:
-console.log(adventurer.belongings[0]);
+//Access the companion's name:
+console.log(adventurer.companion.name);
 
-//ITERATE OVER AN ARRAY WITHIN AN OBJECT
-for (let i=0; i < adventurer.belongings.length; i++) {
-	console.log(adventurer.belongings[i]);
-}
+//Access the companion's type:
+console.log(adventurer.companion.type)
